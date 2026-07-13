df21ae687d6d25783717c70502dd9064d0328e38
amit-mozar <amit1406mozar@gmail.com>
2026-07-13 23:53:49 +0530

    This are leetcode SQL problems

---
+++ leet_code_problem_177_get_nth_highest_salary.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    select coalesce((
+        select temp.salary  from(
+        select *, dense_rank() over(order by e.salary desc) as dr
+        from Employee e
+        ) as temp
+        where temp.dr = N 
+        limit 1
+    ), NULL
+   ) as getNthHighestSalary
+      
+  );
+END;
+$$ LANGUAGE plpgsql;
